933cac53138034a8cd269ff50a2ad7d185f5e29f
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-05-26 19:27:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.50.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  client_id       = "ce1425fc-f3d9-4afa-a1df-9786245f41fc"
-  client_secret   = "JoF8Q~~rOJRfHZ_VubpKKQMi7eqIymQMvdYPTb9."
-  tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
-  subscription_id = "6e4924ab-b00c-468f-ae01-e5d33e8786f8"
-
-}
-
-# Terraform backend state
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-centralindia"
-    storage_account_name = "csg10032000825eeb72"
-    container_name       = "tfstate"
-    key                  = "multistorage.terraform.tfstate"
-  }
-}
-
 # Create multiple storage accounts
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
